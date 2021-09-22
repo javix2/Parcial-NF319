5477d7d9d32773636c337163f04a07a342c0e6ce
javix2 <91162997+javix2@users.noreply.github.com>
2021-09-22 17:30:50 -0300

    Add files via upload

---
+++ p4/listasParImpar/Program.fs
@@ -0,0 +1,18 @@
+﻿// Learn more about F# at http://fsharp.org
+// See the 'F# Tutorial' project for more help.
+open System
+[<EntryPoint>]
+let main argv = 
+    //printfn "%A" argv
+
+    printfn "------------EJERCICIO LISTAS----------"
+    let lista = [1..10]
+    let lpares = List.filter(fun n-> (n % 2) = 0 )lista 
+    let limpares = List.filter(fun n-> (n % 2) = 1 )lista
+    
+    printfn "%A" lista
+    printfn "%A pares " lpares 
+    printfn "%A impares " limpares
+    
+    Console.ReadKey() |>ignore
+    0 // return an integer exit code
